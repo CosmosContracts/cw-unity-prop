09e97870b4b21291cdb07878f7592c0134b13ef0
Alex Lynham <alex@lynh.am>
2022-03-26 11:04:54 +0000

    Add some scaffolding for integration tests

--- src/integration_tests.rs
+++ src/integration_tests.rs
@@ -1,71 +1,96 @@
-// #[cfg(test)]
-// mod tests {
-//     use crate::helpers::CwTemplateContract;
-//     use crate::msg::InstantiateMsg;
-//     use cosmwasm_std::{Addr, Coin, Empty, Uint128};
-//     use cw_multi_test::{App, AppBuilder, Contract, ContractWrapper, Executor};
+#[cfg(test)]
+mod tests {
+    use crate::helpers::CwTemplateContract;
+    use crate::msg::InstantiateMsg;
+    use cosmwasm_std::{Addr, Coin, Empty, Uint128};
+    use cw_multi_test::{App, AppBuilder, Contract, ContractWrapper, Executor};
 
-//     pub fn contract_template() -> Box<dyn Contract<Empty>> {
-//         let contract = ContractWrapper::new(
-//             crate::contract::execute,
-//             crate::contract::instantiate,
-//             crate::contract::query,
-//         );
-//         Box::new(contract)
-//     }
+    pub fn contract_template() -> Box<dyn Contract<Empty>> {
+        let contract = ContractWrapper::new(
+            crate::contract::execute,
+            crate::contract::instantiate,
+            crate::contract::query,
+        );
+        Box::new(contract)
+    }
 
-//     const USER: &str = "USER";
-//     const ADMIN: &str = "ADMIN";
-//     const NATIVE_DENOM: &str = "denom";
+    const USER: &str = "USER";
+    const ADMIN: &str = "ADMIN";
+    const NATIVE_DENOM: &str = "ujuno";
 
-//     fn mock_app() -> App {
-//         AppBuilder::new().build(|router, _, storage| {
-//             router
-//                 .bank
-//                 .init_balance(
-//                     storage,
-//                     &Addr::unchecked(USER),
-//                     vec![Coin {
-//                         denom: NATIVE_DENOM.to_string(),
-//                         amount: Uint128::new(1),
-//                     }],
-//                 )
-//                 .unwrap();
-//         })
-//     }
+    fn mock_app() -> App {
+        AppBuilder::new().build(|router, _, storage| {
+            router
+                .bank
+                .init_balance(
+                    storage,
+                    &Addr::unchecked(USER),
+                    vec![Coin {
+                        denom: NATIVE_DENOM.to_string(),
+                        amount: Uint128::new(3_000_000),
+                    }],
+                )
+                .unwrap();
+        })
+    }
 
-//     fn proper_instantiate() -> (App, CwTemplateContract) {
-//         let mut app = mock_app();
-//         let cw_template_id = app.store_code(contract_template());
+    fn mock_instantiate() -> (App, CwTemplateContract) {
+        let mut app = mock_app();
+        let cw_template_id = app.store_code(contract_template());
 
-//         let msg = InstantiateMsg { count: 1i32 };
-//         let cw_template_contract_addr = app
-//             .instantiate_contract(
-//                 cw_template_id,
-//                 Addr::unchecked(ADMIN),
-//                 &msg,
-//                 &[],
-//                 "test",
-//                 None,
-//             )
-//             .unwrap();
+        let withdraw_address = String::from("gordon-gekko-address"); // in reality this would be e.g. juno16g2rahf5846rxzp3fwlswy08fz8ccuwk03k57y
+        let _validated_addr = Addr::unchecked(&withdraw_address);
+        let withdraw_delay_in_days = 28; // this is what we are expecting to set it to
 
-//         let cw_template_contract = CwTemplateContract(cw_template_contract_addr);
+        let msg = InstantiateMsg {
+            withdraw_address,
+            withdraw_delay_in_days,
+            native_denom: NATIVE_DENOM.to_string(),
+        };
 
-//         (app, cw_template_contract)
-//     }
+        let cw_template_contract_addr = app
+            .instantiate_contract(
+                cw_template_id,
+                Addr::unchecked(ADMIN), // in reality we would set --no-admin
+                &msg,
+                &[],
+                "test",
+                None,
+            )
+            .unwrap();
 
-//     mod count {
-//         use super::*;
-//         use crate::msg::ExecuteMsg;
+        let cw_template_contract = CwTemplateContract(cw_template_contract_addr);
 
-//         #[test]
-//         fn count() {
-//             let (mut app, cw_template_contract) = proper_instantiate();
+        (app, cw_template_contract)
+    }
 
-//             let msg = ExecuteMsg::Increment {};
-//             let cosmos_msg = cw_template_contract.call(msg).unwrap();
-//             app.execute(Addr::unchecked(USER), cosmos_msg).unwrap();
-//         }
-//     }
-// }
+    mod start_withdraw {
+        use super::*;
+        use crate::msg::ExecuteMsg;
+
+        #[test]
+        fn start_withdraw() {
+            let (mut app, cw_template_contract) = mock_instantiate();
+
+            let withdraw_address = String::from("gordon-gekko-address");
+            let validated_addr = Addr::unchecked(&withdraw_address);
+
+            let msg = ExecuteMsg::StartWithdraw {};
+            let cosmos_msg = cw_template_contract.call(msg).unwrap();
+            app.execute(validated_addr, cosmos_msg).unwrap();
+        }
+
+        #[test]
+        fn start_withdraw_fails_with_wrong_address() {
+            let (mut app, cw_template_contract) = mock_instantiate();
+
+            let msg = ExecuteMsg::StartWithdraw {};
+            let cosmos_msg = cw_template_contract.call(msg).unwrap();
+
+            // we expect this to fail
+            // hence unwrap_err
+            app.execute(Addr::unchecked("some-random-address"), cosmos_msg)
+                .unwrap_err();
+        }
+    }
+}
