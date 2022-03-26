84196c4ef8134bbf79aa8de339d34a2347ab450d
Alex Lynham <alex@lynh.am>
2022-03-26 12:08:08 +0000

    Add cw-multi-test examples for basic sudo functionality

--- src/integration_tests.rs
+++ src/integration_tests.rs
@@ -1,9 +1,10 @@
 #[cfg(test)]
 mod tests {
     use crate::helpers::CwTemplateContract;
-    use crate::msg::{InstantiateMsg, QueryMsg, WithdrawalReadyResponse};
-    use cosmwasm_std::{Addr, Coin, Empty, Uint128};
-    use cw_multi_test::{App, AppBuilder, Contract, ContractWrapper, Executor};
+    use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg, SudoMsg, WithdrawalReadyResponse};
+
+    use cosmwasm_std::{coins, Addr, Coin, Empty, Uint128};
+    use cw_multi_test::{App, AppBuilder, AppResponse, Contract, ContractWrapper, Executor};
 
     pub fn contract_template() -> Box<dyn Contract<Empty>> {
         let contract = ContractWrapper::new(
@@ -16,7 +17,7 @@ mod tests {
     }
 
     const USER: &str = "USER";
-    const ADMIN: &str = "ADMIN";
+    //const ADMIN: &str = "ADMIN";
     const NATIVE_DENOM: &str = "ujuno";
 
     fn mock_app() -> App {
@@ -52,10 +53,43 @@ mod tests {
         let cw_template_contract_addr = app
             .instantiate_contract(
                 cw_template_id,
-                Addr::unchecked(ADMIN), // in reality we would set --no-admin
+                Addr::unchecked(USER), // in reality we would set --no-admin
+                &msg,
+                &[Coin {
+                    denom: NATIVE_DENOM.to_string(),
+                    amount: Uint128::new(3_000_000),
+                }], // set a contract balance
+                "cw-unity-prop",
+                None,
+            )
+            .unwrap();
+
+        let cw_template_contract = CwTemplateContract(cw_template_contract_addr.clone());
+
+        (app, cw_template_contract, cw_template_contract_addr)
+    }
+
+    fn mock_instantiate_no_balance() -> (App, CwTemplateContract, Addr) {
+        let mut app = mock_app();
+        let cw_template_id = app.store_code(contract_template());
+
+        let withdraw_address = String::from("gordon-gekko-address"); // in reality this would be e.g. juno16g2rahf5846rxzp3fwlswy08fz8ccuwk03k57y
+        let _validated_addr = Addr::unchecked(&withdraw_address);
+        let withdraw_delay_in_days = 28; // this is what we are expecting to set it to
+
+        let msg = InstantiateMsg {
+            withdraw_address,
+            withdraw_delay_in_days,
+            native_denom: NATIVE_DENOM.to_string(),
+        };
+
+        let cw_template_contract_addr = app
+            .instantiate_contract(
+                cw_template_id,
+                Addr::unchecked(USER), // in reality we would set --no-admin
                 &msg,
                 &[],
-                "test",
+                "cw-unity-prop",
                 None,
             )
             .unwrap();
@@ -72,9 +106,8 @@ mod tests {
         result
     }
 
-    mod start_withdraw {
+    mod withdraw {
         use super::*;
-        use crate::msg::ExecuteMsg;
 
         #[test]
         fn start_withdraw() {
@@ -108,6 +141,86 @@ mod tests {
             // hence unwrap_err
             app.execute(Addr::unchecked("some-random-address"), cosmos_msg)
                 .unwrap_err();
+        }
+    }
+
+    fn exec_sudo_burn(app: &mut App, contract_address: Addr) -> anyhow::Result<AppResponse> {
+        let msg = SudoMsg::ExecuteBurn {};
+        app.wasm_sudo(contract_address, &msg)
+    }
+
+    fn exec_sudo_send(
+        app: &mut App,
+        contract_address: Addr,
+        recipient: String,
+        amount: Uint128,
+    ) -> anyhow::Result<AppResponse> {
+        let msg = SudoMsg::ExecuteSend { recipient, amount };
+        app.wasm_sudo(contract_address, &msg)
+    }
+
+    fn get_balance(app: &mut App, address: &Addr) -> Vec<Coin> {
+        app.wrap().query_all_balances(address).unwrap()
+    }
+
+    mod sudo {
+        use super::*;
+
+        #[test]
+        fn sudo_burn() {
+            let (mut app, _cw_template_contract, contract_addr) = mock_instantiate();
+
+            // this tests for success
+            exec_sudo_burn(&mut app, contract_addr.clone()).unwrap();
+
+            let contract_balance = get_balance(&mut app, &contract_addr);
+
+            assert_eq!(contract_balance, &[]);
+        }
+
+        #[test]
+        fn sudo_send() {
+            let (mut app, _cw_template_contract, contract_addr) = mock_instantiate();
+
+            let nominated_address = String::from("carl-fox-address");
+            let validated_addr = Addr::unchecked(&nominated_address);
+
+            // this tests for success
+            exec_sudo_send(
+                &mut app,
+                contract_addr.clone(),
+                validated_addr.to_string(),
+                Uint128::new(2_000_000),
+            )
+            .unwrap();
+
+            let community_nominated_address_balance = get_balance(&mut app, &validated_addr);
+
+            assert_eq!(
+                community_nominated_address_balance,
+                coins(2_000_000, NATIVE_DENOM),
+            );
+
+            let contract_balance = get_balance(&mut app, &contract_addr);
+
+            assert_eq!(contract_balance, coins(1_000_000, NATIVE_DENOM),);
+        }
+
+        #[test]
+        fn sudo_send_fails() {
+            let (mut app, _cw_template_contract, contract_addr) = mock_instantiate_no_balance();
+
+            let nominated_address = String::from("carl-fox-address");
+            let validated_addr = Addr::unchecked(&nominated_address);
+
+            // this tests for error
+            let _err = exec_sudo_send(
+                &mut app,
+                contract_addr,
+                validated_addr.to_string(),
+                Uint128::new(2_000_000),
+            )
+            .unwrap_err();
         }
     }
 }
